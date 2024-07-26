68721f1afb0e6a76965453c345fd86e2aebe3a08
Kirby Zabala <jkirbyzabala@gmail.com>
2024-07-26 11:51:56 -0600

    added comments to the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import FavoritesPage from './pages/FavoritesPage';
-import DoNotRecommendPage from './pages/DoNotRecommendPage';
-import MapPage from './pages/MapPage';
-import Header from './components/Header';
+// Import necessary modules
+import React from 'react'; // Import React for building the UI
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import Router and Route components for routing
+import HomePage from './pages/HomePage'; // Import the HomePage component
+import FavoritesPage from './pages/FavoritesPage'; // Import the FavoritesPage component
+import DoNotRecommendPage from './pages/DoNotRecommendPage'; // Import the DoNotRecommendPage component
+import MapPage from './pages/MapPage'; // Import the MapPage component
+import Header from './components/Header'; // Import the Header component
 
 const App = () => {
   return (
     <Router>
+      {/* Render the Header component on all pages */}
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/do-not-recommend" element={<DoNotRecommendPage />} />
-        <Route path="/map" element={<MapPage />} />
-      </Routes>
+      <Switch>
+        {/* Define routes for different pages */}
+        <Route exact path="/" component={HomePage} />
+        <Route path="/favorites" component={FavoritesPage} />
+        <Route path="/do-not-recommend" component={DoNotRecommendPage} />
+        <Route path="/map" component={MapPage} />
+      </Switch>
     </Router>
   );
 };
